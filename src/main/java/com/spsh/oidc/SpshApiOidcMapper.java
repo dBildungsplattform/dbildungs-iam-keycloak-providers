5c8bfefb842c30eb6ed7b89b0f3c281ddf7e2436
Caspar Neumann <146704428+casparneumann-cap@users.noreply.github.com>
2025-01-29 11:44:49 +0100

    SPSH-1515 External Data & API-Key Communication (#1)

---
+++ src/main/java/com/spsh/oidc/SpshApiOidcMapper.java
@@ -0,0 +1,147 @@
+package com.spsh.oidc;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.io.entity.StringEntity;
+import org.jboss.logging.Logger;
+import org.keycloak.models.ClientSessionContext;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.ProtocolMapperModel;
+import org.keycloak.models.UserSessionModel;
+import org.keycloak.protocol.oidc.mappers.AbstractOIDCProtocolMapper;
+import org.keycloak.protocol.oidc.mappers.OIDCAccessTokenMapper;
+import org.keycloak.protocol.oidc.mappers.OIDCAttributeMapperHelper;
+import org.keycloak.protocol.oidc.mappers.OIDCIDTokenMapper;
+import org.keycloak.protocol.oidc.mappers.UserInfoTokenMapper;
+import org.keycloak.provider.ProviderConfigProperty;
+import org.keycloak.representations.IDToken;
+import com.jayway.jsonpath.JsonPath;
+
+public class SpshApiOidcMapper extends AbstractOIDCProtocolMapper implements OIDCAccessTokenMapper, OIDCIDTokenMapper, UserInfoTokenMapper {
+
+
+    public static final String ENV_KEY_INTERNAL_COMMUNICATION_API_KEY = "INTERNAL_COMMUNICATION_API_KEY";
+    public static final String PROVIDER_ID = "spsh-custom-oidc-api-mapper";
+    public static final String FETCH_URL = "fetchUrl";
+    public static final String EXTRACT_JSON_PATH = "extractJsonPath";
+    private static final List<ProviderConfigProperty> configProperties = new ArrayList<>();
+    private static final Logger LOGGER = Logger.getLogger(SpshApiOidcMapper.class);
+
+    static {
+        OIDCAttributeMapperHelper.addTokenClaimNameConfig(configProperties);
+        OIDCAttributeMapperHelper.addIncludeInTokensConfig(configProperties, SpshApiOidcMapper.class);
+
+        ProviderConfigProperty fetchUrlProperty = new ProviderConfigProperty();
+        fetchUrlProperty.setName(FETCH_URL);
+        fetchUrlProperty.setLabel("SPSH Fetch Url");
+        fetchUrlProperty.setType(ProviderConfigProperty.STRING_TYPE);
+        fetchUrlProperty.setHelpText("The URL to fetch data from the SPSH Backend.");
+        configProperties.add(fetchUrlProperty);
+
+        ProviderConfigProperty extractPathProperty = new ProviderConfigProperty();
+        extractPathProperty.setName(EXTRACT_JSON_PATH);
+        extractPathProperty.setLabel("SPSH Extract Json Path");
+        extractPathProperty.setType(ProviderConfigProperty.STRING_TYPE);
+        extractPathProperty.setHelpText("The JSON path to extract data from the API response.");
+        configProperties.add(extractPathProperty);
+    }
+
+    @Override
+    public String getDisplayCategory() {
+        return "Token Mapper";
+    }
+
+    @Override
+    public String getDisplayType() {
+        return "SPSH Custom OIDC Api Mapper";
+    }
+
+    @Override
+    public String getHelpText() {
+        return "The mapper calls the provided SPSH fetch url, extracts the provided JsonPath from the api response and maps the result if not null to the claim";
+    }
+
+    @Override
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return configProperties;
+    }
+
+    @Override
+    public String getId() {
+        return PROVIDER_ID;
+    }
+
+    @Override
+    protected void setClaim(IDToken token, ProtocolMapperModel mappingModel,
+      UserSessionModel userSession, KeycloakSession keycloakSession,
+      ClientSessionContext clientSessionCtx) {
+        String fetchUrl = mappingModel.getConfig().get(FETCH_URL);
+        String extractJsonPath = mappingModel.getConfig().get(EXTRACT_JSON_PATH);
+        String userSub = userSession.getUser().getId();
+
+        LOGGER.info(String.format("Setting claims via custom SpshApiOidcMapper for userSub: %s", userSub));
+        LOGGER.debug(String.format("Using fetchUrl: %s", fetchUrl));
+        LOGGER.debug(String.format("Using extractJsonPath: %s", extractJsonPath));
+        LOGGER.debug(String.format("Using userSub: %s", userSub));
+
+        if (fetchUrl == null) {
+            LOGGER.warn("SpshApiOidcMapper: fetchUrl is null. No data will be fetched, extracted and mapped.");
+            return;
+        }
+        if (extractJsonPath == null) {
+            LOGGER.warn("SpshApiOidcMapper: extractJsonPath is null. No data will be fetched, extracted and mapped.");
+            return;
+        }
+        if (userSub == null) {
+            LOGGER.warn("SpshApiOidcMapper: userSub is null. No data will be fetched, extracted and mapped.");
+            return;
+        }
+
+        try {
+            String responseData = fetchApiData(fetchUrl, userSub);
+            String extractedValue = extractFromJson(responseData, extractJsonPath);
+            if (extractedValue != null) {
+                OIDCAttributeMapperHelper.mapClaim(token, mappingModel, extractedValue);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private String fetchApiData(String url, String userSub) throws IOException {
+
+        String apiKey = System.getenv(ENV_KEY_INTERNAL_COMMUNICATION_API_KEY);
+        if (apiKey == null || apiKey.isEmpty()) {
+            throw new IOException(String.format("Environment variable %s is not set or is empty.", ENV_KEY_INTERNAL_COMMUNICATION_API_KEY));
+        }
+
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpPost request = new HttpPost(url);
+            request.setHeader("Content-Type", "application/json");
+            request.setHeader("api-key", apiKey);
+            StringEntity requestBody = new StringEntity(String.format("{\"sub\":\"%s\"}", userSub));
+            request.setEntity(requestBody);
+
+            return httpClient.execute(request, response -> {
+                int statusCode = response.getCode();
+                if (statusCode >= 200 && statusCode < 300) {
+                    HttpEntity entity = response.getEntity();
+                    return entity != null ? EntityUtils.toString(entity) : null;
+                } else {
+                    throw new IOException("Unexpected response status: " + statusCode);
+                }
+            });
+        }
+    }
+
+    private String extractFromJson(String jsonData, String jsonPath) {
+            return JsonPath.read(jsonData, jsonPath).toString();
+    }
+}
